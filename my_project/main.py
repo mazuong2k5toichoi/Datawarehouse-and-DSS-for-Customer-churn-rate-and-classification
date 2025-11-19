0504df80ee8cadae4611ed0b1bffe469f6fbed44
Haiden <dang,tranhai2005@gmail.com>
2025-11-19 10:49:55 +0700

    feat: test extract and load data

--- my_project/main.py
+++ my_project/main.py
@@ -1,6 +1,11 @@
-def main():
-    print("Hello from my-project!")
+import sys
+from elt_pipeline.ingest.psql_ingest import extract_data_from_sql, load_data_to_postgres
 
+def main():
+    query = "SELECT  * FROM AdventureWorks2022.Person.Person"
+    data = extract_data_from_sql(query)
+    # print(f"Extracted {len(data)} records from source database {data}.")
+    load_data_to_postgres(data, table_name="person_data")
 
 if __name__ == "__main__":
     main()
