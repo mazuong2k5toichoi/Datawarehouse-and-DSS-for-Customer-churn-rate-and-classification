fcba717474faaea80813a1e9d5ec3b3cd277e786
DankoFox <timzek7@gmail.com>
2025-11-24 17:48:34 +0700

    add(silver-staging): more info on territory

--- my_project/elt_pipeline/abc/models/silver/staging/stg_sales__sales_territory.sql
+++ my_project/elt_pipeline/abc/models/silver/staging/stg_sales__sales_territory.sql
@@ -1,7 +1,14 @@
 -- models/silver/staging/stg_sales__sales_territory.sql
 with source as (
     select
-        "TerritoryID" as territory_id
+        "TerritoryID" as territory_id,
+        "Name" as name,
+        "CountryRegionCode" as country_region_code,
+        "Group" as "group",
+        "SalesYTD" as sales_ytd,
+        "SalesLastYear" as sales_last_year,
+        "CostYTD" as cost_ytd,
+        "CostLastYear" as cost_last_year
     from {{ source('bronze_adventureworks', 'sales_sales_territory') }}
 ),
 
@@ -15,6 +22,13 @@ with_loaddate as (
 derived_columns as (
     select
         territory_id,
+        name,
+        country_region_code,
+        "group",
+        sales_ytd,
+        sales_last_year,
+        cost_ytd,
+        cost_last_year,
         loaddate,
         {{ dbt_utils.generate_surrogate_key(['territory_id']) }} as territory_hk,
         'Sales.SalesTerritory' as recordsource
