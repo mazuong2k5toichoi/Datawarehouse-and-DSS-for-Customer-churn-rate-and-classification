ae804fa975fe82158efd84370e30f4495cb79341
DankoFox <timzek7@gmail.com>
2025-11-25 15:34:51 +0700

    edit(gold-dim): limit dim_date

--- my_project/elt_pipeline/abc/models/gold/dims/dim_date.sql
+++ my_project/elt_pipeline/abc/models/gold/dims/dim_date.sql
@@ -9,8 +9,8 @@
 with date_spine as (
     {{ dbt_utils.date_spine(
         datepart="day",
-        start_date="cast('2000-01-01' as date)",
-        end_date="cast('2030-12-31' as date)"
+        start_date="cast('2010-01-01' as date)",
+        end_date="cast('2014-12-31' as date)"
     ) }}
 ),
 
